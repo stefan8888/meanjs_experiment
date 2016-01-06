02d00d86f3394b52e4b1f328c351185b0fb95815
Standard <standard@McMe.local>
2016-01-06 17:09:03 +0100

    Added new alerts to controller

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,22 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $scope.alerts = [
+      {
+        icon: 'glyphicon-user',
+        colour: 'btn-success',
+        total: '20,408',
+        description: 'TOTAL CUSTOMERS'
+      },
+
+      {
+        icon: 'glyphicon-calendar',
+        colour: 'btn-primary',
+        total: '8,382',
+        description: 'UPCOMING EVENTS'
+      }
+
+    ];
   }
 ]);
